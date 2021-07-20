64750c0d15f18a49c53914d386ca87664e620627
muhai <muhai.li@tuya.com>
2021-07-20 18:00:45 +0800

    fix: ShieldsValue

--- src/builtins/ShieldsValue.tsx
+++ src/builtins/ShieldsValue.tsx
@@ -3,31 +3,15 @@ import './ShieldsValue.less';
 import { Skeleton } from 'antd';
 import React, { useEffect, useState } from 'react';
 
-const parseSvg = async (url: string) => {
-  const html = await fetch(
-    `https://cors-anywhere.herokuapp.com/${url}`,
-  ).then(res => res.text());
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'image/svg+xml');
-  const g =
-    doc.querySelector('svg > g:last-child') ??
-    doc.querySelector('svg > g:nth-child(4)');
-  const label = g.querySelector('text:first-child')?.textContent;
-  const value = g.querySelector('text:last-child')?.textContent;
-  return {
-    label,
-    value,
-  };
-};
-
-export default ({ data, href }) => {
+export default ({ data, href, name }) => {
   const [text, setText] = useState('');
   const [loading, setLoading] = useState(false);
 
   const init = async () => {
     try {
       setLoading(true);
-      const result = await parseSvg(data);
+      const store = await fetch(data).then(res => res.json());
+      const result = store[name];
       setText(`${result.label}：${result.value}`);
     } catch (error) {
       console.log(error);
