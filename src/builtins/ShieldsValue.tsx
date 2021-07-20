f90fdc10675e1cccb422091f95b338a84ea805ff
muhai <muhai.li@tuya.com>
2021-07-20 18:28:06 +0800

    feat: cache

--- src/builtins/ShieldsValue.tsx
+++ src/builtins/ShieldsValue.tsx
@@ -1,6 +1,8 @@
 import { Skeleton } from 'antd';
 import React, { useEffect, useState } from 'react';
 
+let cache = null;
+
 export default ({ data, href, name }) => {
   const [text, setText] = useState('');
   const [loading, setLoading] = useState(false);
@@ -8,7 +10,15 @@ export default ({ data, href, name }) => {
   const init = async () => {
     try {
       setLoading(true);
-      const store = await fetch(`${data}?t=${Date.now()}`).then(res => res.json());
+
+      let store = {};
+      if (cache) {
+        store = cache;
+      } else {
+        store = await fetch(`${data}?t=${Date.now()}`).then(res => res.json());
+        cache = store;
+      }
+
       const result = store[name];
       setText(`${result.label}：${result.value}`);
     } catch (error) {
