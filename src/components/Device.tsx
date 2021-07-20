5a34c5855901a3207453c894169d9593485894aa
muhai <muhai.li@tuya.com>
2021-07-20 19:08:11 +0800

    feat: add device loading

--- src/components/Device.tsx
+++ src/components/Device.tsx
@@ -5,6 +5,7 @@ import QRCode from 'qrcode.react';
 import React, { FC, useContext, useEffect, useState } from 'react';
 
 import { appendParams } from '../utils';
+import { Spin } from 'antd';
 
 interface IDeviceProps {
   className?: string;
@@ -43,51 +44,57 @@ const Device: FC<IDeviceProps> = ({
     };
   }, []);
 
+  const [loading, setLoading] = useState(true);
+
   return (
-    <div
-      className={['__dumi-default-device'].concat(className).join(' ')}
-      data-device-type="iOS"
-      data-mode={mode}
-    >
-      <div className="device_contain">
-        <div className="__dumi-default-device-status">
-          <span>{title}</span>
-          <span>{`${dateTime
-            .getHours()
-            .toString()
-            .padStart(2, '0')}:${dateTime
-            .getMinutes()
-            .toString()
-            .padStart(2, '0')}`}</span>
-        </div>
-        <iframe
-          ref={forwardRef}
-          title="dumi-previewer"
-          src={url}
-          key={renderKey}
-        />
-        <div className="__dumi-default-device-action">
-          <button
-            className="__dumi-default-icon"
-            role="refresh"
-            onClick={() => setRenderKey(Math.random())}
+    <Spin spinning={loading} delay={300}>
+      <div
+        className={['__dumi-default-device'].concat(className).join(' ')}
+        data-device-type="iOS"
+        data-mode={mode}
+      >
+        <div className="device_contain">
+          <div className="__dumi-default-device-status">
+            <span>{title}</span>
+            <span>{`${dateTime
+              .getHours()
+              .toString()
+              .padStart(2, '0')}:${dateTime
+              .getMinutes()
+              .toString()
+              .padStart(2, '0')}`}</span>
+          </div>
+          <iframe
+            ref={forwardRef}
+            title="dumi-previewer"
+            src={url}
+            key={renderKey}
+            onLoad={() => setLoading(false)}
+            onError={() => setLoading(false)}
           />
-          <button className="__dumi-default-icon" role="qrcode">
-            <QRCode
-              value={qrcode ? appendParams(qrcode, { demo }) : url}
-              size={96}
+          <div className="__dumi-default-device-action">
+            <button
+              className="__dumi-default-icon"
+              role="refresh"
+              onClick={() => setRenderKey(Math.random())}
             />
-          </button>
-          <a
-            href={url}
-            target="_blank"
-            rel="noreferrer"
-            className="__dumi-default-icon"
-            role="open-demo"
-          />
+            <button className="__dumi-default-icon" role="qrcode">
+              <QRCode
+                value={qrcode ? appendParams(qrcode, { demo }) : url}
+                size={96}
+              />
+            </button>
+            <a
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="__dumi-default-icon"
+              role="open-demo"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </Spin>
   );
 };
 
