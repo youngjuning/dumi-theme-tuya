5fcb34288e4c8b97b648a9d78efc65b37d07e4b3
muhai <muhai.li@tuya.com>
2021-11-17 12:25:19 +0800

    feat: slugs stylefix

--- src/components/SlugList.tsx
+++ src/components/SlugList.tsx
@@ -1,25 +1,50 @@
 import './SlugList.less';
 
-import { AnchorLink } from 'dumi/theme';
-import React, { FC } from 'react';
+import { AnchorLink, context } from 'dumi/theme';
+import React, { FC, useContext } from 'react';
+import { classnames } from '../utils';
 
 import { getArray } from '../utils';
+
+const getHash = () => {
+  if (typeof window !== 'undefined') {
+    const hash = window.location.hash;
+    if (typeof hash === 'string') {
+      return decodeURIComponent(hash.slice(1));
+    }
+  }
+};
 
 const SlugsList: FC<{ slugs: any; className?: string }> = ({
   slugs,
   ...props
-}) => (
-  <ul role="slug-list" {...props}>
-    {getArray<any>(slugs)
-      .filter(({ depth }) => depth <= 2)
-      .map(slug => (
-        <li key={slug.heading} title={slug.value} data-depth={slug.depth}>
-          <AnchorLink to={`#${slug.heading}`}>
-            <span>{slug.value}</span>
-          </AnchorLink>
-        </li>
-      ))}
-  </ul>
-);
+}) => {
+  const {
+    meta: { title },
+  } = useContext(context);
+  const hash = getHash() || slugs?.[0]?.heading;
+  return (
+    <>
+      <ul role="slug-list" {...props}>
+        <li role="slug-list-title">{title}</li>
+        {getArray<any>(slugs)
+          .filter(slug => slug.depth < 4)
+          .map(slug => (
+            <li key={slug.heading} title={slug.value} data-depth={slug.depth}>
+              <a
+                href={`#${slug.heading}`}
+                className={classnames({ active: hash === slug.heading })}
+                onClick={() => {
+                  AnchorLink.scrollToAnchor(slug.heading);
+                }}
+              >
+                <span>{slug.value}</span>
+              </a>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
 
 export default SlugsList;
