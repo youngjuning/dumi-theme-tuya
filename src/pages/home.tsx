835b01cac1b6540a6e223eea9571a3b82b81646e
muhai <muhai.li@tuya.com>
2022-10-09 17:28:44 +0800

    feat: hot link

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import { context, Link } from 'dumi/theme';
 import React, { ReactNode, useContext } from 'react';
+import { FlushButton } from '../components/flush-button';
 
 import { getArray, join } from '../utils';
 
@@ -10,6 +11,7 @@ interface HomeMeta {
   actions: Array<{
     link: string;
     text: string;
+    hot?: boolean;
   }>;
   footer?: string;
   background?: string;
@@ -17,20 +19,23 @@ interface HomeMeta {
 
 export const Home = ({ content }: { content?: ReactNode }) => {
   const { meta, base } = useContext(context);
-  const { image, title, desc, actions, footer } = (meta?.hero ?? {}) as HomeMeta;
+  const { image, title, desc, actions, footer } = (meta?.hero ??
+    {}) as HomeMeta;
   return (
     <div className={'__dumi-default-layout-home'}>
-      <div
-        className={'__dumi-default-layout-hero'}
-      >
+      <div className={'__dumi-default-layout-hero'}>
         {image ? <img src={image} /> : null}
         <h1>{title}</h1>
         <div dangerouslySetInnerHTML={{ __html: desc }} />
-        {getArray(actions).map(action => (
-          <Link to={join(base, action.link)} key={action.text}>
-            <button type="button">{action.text}</button>
-          </Link>
-        ))}
+        {getArray(actions).map(action =>
+          action.hot ? (
+            <FlushButton text={action.text} link={action.link} />
+          ) : (
+            <Link to={join(base, action.link)} key={action.text}>
+              <button type="button">{action.text}</button>
+            </Link>
+          ),
+        )}
       </div>
       <div
         style={{
